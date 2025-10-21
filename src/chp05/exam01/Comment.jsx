4b958faa946b834d99e4022f46fdc4a76be7a9c1
Goldstargreat <goldstargreat@gmail.com>
2025-10-21 13:20:51 +0900

    5장 수정

--- src/chp05/exam01/Comment.jsx
+++ src/chp05/exam01/Comment.jsx
@@ -3,23 +3,23 @@ import UserInfo from "./UserInfo";
 import './Comment.css'
 const users = [
     {
-        name: "홍금보",
-        comment: "날라차기를 합니다.",
-        avatarUrl: "https://www.afa-academy.com/wp-content/uploads/2025/01/18th-Sammo-HUNG.jpg "
+        name: "장원영",
+        comment: "초긍정마인드, 럭키비키",
+        avatarUrl: "https://cdn.pixabay.com/photo/2025/08/28/11/47/user-9801872_1280.png"
     },
     {
-        name: "견자단",
-        comment: "영춘권을 합니다.",
-        avatarUrl: "https://cdn.topstarnews.net/news/photo/201804/397809_43372_1513.jpg "
+        name: "안유진",
+        comment: "모두가 잘 좋아한다.~~",
+        avatarUrl: "https://cdn.pixabay.com/photo/2025/08/28/11/47/user-9801872_1280.png"
     },
     {
-        name: "트럼프",
-        comment: "마약선을 폭격한다.",
-        avatarUrl: "https://upload.wikimedia.org/wikipedia/commons/1/16/Official_Presidential_Portrait_of_President_Donald_J._Trump_%282025%29.jpg "
+        name: "가을~~",
+        comment: "아이브의 신곡이 나왔어요.",
+        avatarUrl: "https://cdn.pixabay.com/photo/2025/08/28/11/47/user-9801872_1280.png"
     }
 ];
 
-function Comment() {
+function Comment(props) {
     const currentDate = new Date();
     return(
       <div>
